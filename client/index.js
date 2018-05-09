57fcf245a60f40f3b5e447721a8a3d662b5c6b3e
DemonoiD <daemon84@mail.ru>
2018-05-09 21:38:23 +0300

    apollo client add

--- client/index.js
+++ client/index.js
@@ -2,14 +2,59 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App'
 import router from '@router/index'
-// import appRouter from '@router/appRouter'
 import store from '@store'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
+import VueApollo from 'vue-apollo'
+
+const httpLink = new HttpLink({
+    // You should use an absolute URL here
+    uri: 'http://localhost:8989/graphql'
+})
+
+// Create the subscription websocket link
+const wsLink = new WebSocketLink({
+    uri: 'ws://localhost:3000/subscriptions',
+    options: {
+        reconnect: true
+    }
+})
+
+// using the ability to split links, you can send data to each link
+// depending on what kind of operation is being sent
+const link = split(
+    // split based on operation type
+    ({ query }) => {
+        const { kind, operation } = getMainDefinition(query)
+        return kind === 'OperationDefinition' &&
+            operation === 'subscription'
+    },
+    wsLink,
+    httpLink
+)
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+    link,
+    cache: new InMemoryCache(),
+    connectToDevTools: true
+})
+
+const apolloProvider = new VueApollo({
+    defaultClient: apolloClient
+})
 
 Vue.use(VueRouter)
+Vue.use(VueApollo)
 
 new Vue({
     router: router,
     store: store,
+    provide: apolloProvider.provide(),
     render: h => h(App),
     data: {
         currentPageName: '' // имя текущей страницы
